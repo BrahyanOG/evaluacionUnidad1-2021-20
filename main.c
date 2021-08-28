642baa1a606d0784678be64d07d17e0cff3f8d44
Brahyan Ossa <Brahyan399@gmail.com>
2021-08-28 14:23:03 -0700

    feature 1 y 2

--- main.c
+++ main.c
@@ -3,6 +3,27 @@
 #include "student.h"
 
 int main(int argc, char *argv[]){
+    
+    FILE *inFile = fopen("./fin", "r");
+    FILE *inFileOut = fopen("./fout", "a+");
+    if (inFile == NULL) {
+        perror("Open file fin fails: ");
+        return(EXIT_FAILURE);
+    }
+    if (inFileOut == NULL) {
+        perror("Open file fout fails: ");
+        return(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (i == 1) feature1(inFile, inFileOut);
+        if (i == 2) feature2(inFile, inFileOut);
+    }
+    
+    fclose(inFile);
+    fclose(inFileOut);
+
 
     return EXIT_SUCCESS;
 }
